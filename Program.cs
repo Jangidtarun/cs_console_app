61990a0d9db0f020f774767377acd8374695069b
tarun jangid <tarunjangid19102002@gmail.com>
2023-04-19 05:03:24 +0530

    first thing first

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("what is your name?");
+var name = Console.ReadLine();
+var currentdate = DateTime.Now;
+Console.WriteLine($"Hello, {name}, on {currentdate:d} at {currentdate:t}.");
+Console.Write($"Press any key to exit...");
+Console.ReadKey(true);
